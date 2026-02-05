43ffb4a85318c672c89d73940e99b346a1ee80e7
rohanmangalxb <rohanmangal.2021@gmail.com>
2026-02-05 15:08:30 +0530

    D

--- server/util.py
+++ server/util.py
@@ -1,15 +1,21 @@
-import json, pickle, numpy as np
-
+import json
+import pickle
+import numpy as np
+import os
 
 location_data = None
 columns_data = None
 model_data = None
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 def get_estimated_price(location, sqft, bhk, bath):
     try:
         loc_index = columns_data.index(location.lower())
-    except:
+    except ValueError:
         loc_index = -1
+
     x = np.zeros(len(columns_data))
     x[0] = sqft
     x[1] = bath
@@ -20,26 +26,26 @@ def get_estimated_price(location, sqft, bhk, bath):
 
     return round(model_data.predict([x])[0], 2)
 
+
 def get_location_names():
     return location_data
 
+
 def load_saved_artifacts():
-    print('Loading saved artifacts....')
+    print("Loading saved artifacts...")
+
     global location_data
     global columns_data
     global model_data
 
-    with open("./server/artifacts/columns.json", 'r') as f:
-        columns_data = json.load(f)['data_columns']
+    columns_path = os.path.join(BASE_DIR, "artifacts", "columns.json")
+    model_path = os.path.join(BASE_DIR, "artifacts", "prediction_model.pickle")
+
+    with open(columns_path, "r") as f:
+        columns_data = json.load(f)["data_columns"]
         location_data = columns_data[3:]
-    
-    with open("./server/artifacts/prediction_model.pickle", 'rb') as f:
+
+    with open(model_path, "rb") as f:
         model_data = pickle.load(f)
 
-    print("Loading of artifacts completed!")
-
-if __name__ == "__main__":
-    load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 3))
+    print("Artifacts loaded successfully!")
